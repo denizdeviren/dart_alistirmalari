31509cdf0fc0ff9dde08ec662c6238ff0f81948d
Deniz Deviren <158352894+denizdeviren@users.noreply.github.com>
2025-08-19 12:00:54 +0300

    Add files via upload

---
+++ Dart Giriş/hello_word.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  print("Deniz Deviren");
+  print("Deniz Deviren'in dersleri");
+  print('Deniz Deviren\'in dersleri');
+  print("3*5");
+  print(3*5);
+  print(3+5);
+
+  //metinleri birleştirmedir
+  print("deniz"+"deviren");
+
+  /*
+  Bu toplu satır yorum satırıdır.
+  print("deneme");
+  print("deneme");
+  print("deneme");
+  print("deneme");
+  */
+
+}
